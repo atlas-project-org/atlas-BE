62484593b314c5bfada771400658cef9e8677af7
Brian Walvoord <brian.walvoord@gmail.com>
2022-12-01 14:26:22 +0900

    chore: added comments for mongodb connection with mongoose

--- src/Models/airbnb.js
+++ src/Models/airbnb.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
+// design the schema for DB to use in express
 const AirbnbSchema = new mongoose.Schema({
   name: String,
 });
 
+// export model(chosen name, schema, collection name)
 const Airbnb = mongoose.model("Airbnb", AirbnbSchema, "listingsAndReviews");
 
 module.exports = Airbnb;
--- src/Routes/airbnbRoutes.js
+++ src/Routes/airbnbRoutes.js
@@ -3,6 +3,8 @@ const airbnbModel = require("../Models/airbnb.js");
 const app = express();
 
 app.get("/testme", async (request, response) => {
+  // send query with .find parameters to look for
+  // *note: add .limit(<num>) to set limit
   const placesToStay = await airbnbModel.find({
     name: "Private Room in Bushwick",
   });
